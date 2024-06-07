f64f7f16ad6cb7408facbc973b447bf13b3978b8
ZeroBall526 <96288248+ZeroBall526@users.noreply.github.com>
2024-06-07 21:00:14 +0900

    auto commit by intellij

--- src/main/java/steaming/randombox/main.java
+++ src/main/java/steaming/randombox/main.java
@@ -1,17 +1,21 @@
 package steaming.randombox;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("randombox survival 플러그인이 활성화 되었어요!");
+        getServer().getPluginManager().registerEvents(new radombox() , this);
         // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("randombox survival 플러그인이 비활성화 되었어요!");
         // Plugin shutdown logic
     }
 }
--- src/main/java/steaming/randombox/radombox.java
+++ src/main/java/steaming/randombox/radombox.java
@@ -1,6 +1,18 @@
 package steaming.randombox;
 
-import java.util.EventListener;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
 
-public class radombox implements EventListener {
+
+public class radombox implements Listener {
+
+    @EventHandler
+    public void playercraftevent(CraftItemEvent event){
+        event.setCancelled(true);
+        Player p = (Player) event.getWhoClicked();
+
+        p.sendMessage("크래프팅중!");
+    }
 }
